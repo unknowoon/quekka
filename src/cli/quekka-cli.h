447e58d364d9826da1ebb1bcdaa7c6e2bfcf9cde
jaeseong lee <56257117+dooripapa@users.noreply.github.com>
2025-12-31 23:28:57 +0900

    Create quekka-cli.h header file

---
+++ src/cli/quekka-cli.h
@@ -0,0 +1,12 @@
+//
+// Created by koscom on 2025. 12. 31..
+//
+
+#ifndef QUEKKA_QUEKKA_CLI_H
+#define QUEKKA_QUEKKA_CLI_H
+
+#include "internal/comm.h"
+#include "internal/libsocket.h"
+#include "internal/epoll_handler.h"
+
+#endif //QUEKKA_QUEKKA_CLI_H
